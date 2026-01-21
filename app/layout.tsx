4655ff3c9cd701b6fe0f79d9c3887be9a28988ae
oleh skrypnyk <oleh.skrypnyk@ukr.net>
2026-01-21 20:54:36 +0200

    Upadate layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <TanstackProvider>
+          <div id="modal-root"></div>
           <Header />
           <main className="main">{children}</main>
           <Footer />
